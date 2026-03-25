6cf6a3531bcd99161a124a4da42df019ad820cd6
Himanshu Tiwari <htiwari578@gmail.com>
2026-03-25 22:55:06 +0530

    prisma 7 configiration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { success } from 'zod';
+import { prisma } from './lib/prisma.js';
 
 export const app = express();
 const PORT = process.env.PORT || 4000;
@@ -13,3 +14,15 @@ app.get("/health-check", (req, res) => {
     success: true,
     message: "Server is healthy and running"});
 });
+
+app.get("/test", async(req: Request, res: Response) => {
+    try {
+        const notes = await prisma.note.findMany();
+        res.status(200).json({
+            success: true,
+            data: notes
+        });
+    } catch (error) {
+        console.log(error);
+    }
+})
