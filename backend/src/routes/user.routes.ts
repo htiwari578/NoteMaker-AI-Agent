485ad63b52cc718571ce197cff8579cdd0bb08fc
Himanshu Tiwari <htiwari578@gmail.com>
2026-03-26 23:06:57 +0530

    validate rgisterUser

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { registerUser } from "../controllers/user.controllers.js";
+import { validateData } from "../middleware/validate.middleware.js";
+import { registerUserSchema } from "../validations/auth.validations.js";
 
 const router = express.Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(validateData(registerUserSchema), registerUser);
 export default router;
