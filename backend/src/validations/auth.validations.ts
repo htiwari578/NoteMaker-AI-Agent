183d511793d2f4f59a8ca2d62a6575b2a9107215
Himanshu Tiwari <htiwari578@gmail.com>
2026-03-27 22:58:35 +0530

    auth validation

--- backend/src/validations/auth.validations.ts
+++ backend/src/validations/auth.validations.ts
@@ -1,10 +1,10 @@
 import {z} from 'zod';
 
-export const registerSchema = z.object({
+export const registerUserSchema = z.object({
     name: z.string().min(3, 'Name must be at least 3 characters long'),
     email: z.string().email('Invalid email address'),
     password: z.string().min(6, 'Password must be at least 6 characters long'),
 })
 .strict();
 
-export type RegisterInput = z.infer<typeof registerSchema>;
+export type RegisterInput = z.infer<typeof registerUserSchema>;
