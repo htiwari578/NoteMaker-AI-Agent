d32f73cb094eee819898fef992a995c6a10795ca
Himanshu Tiwari <htiwari578@gmail.com>
2026-03-29 23:43:03 +0530

    changes in zod validation

--- backend/src/validations/auth.validations.ts
+++ backend/src/validations/auth.validations.ts
@@ -1,10 +1,20 @@
 import {z} from 'zod';
 
+//register user schema
 export const registerUserSchema = z.object({
     name: z.string().min(3, 'Name must be at least 3 characters long'),
-    email: z.string().email('Invalid email address'),
+    email: z.string().trim().toLowerCase(),
     password: z.string().min(6, 'Password must be at least 6 characters long'),
 })
 .strict();
 
+//login user schema
+export const loginUserSchema = z.object({
+    email: z.string().min(1, 'Email is required').trim().toLowerCase(),
+    password: z.string().min(1, 'Password is required'),
+})
+.strict();
+
+
 export type RegisterInput = z.infer<typeof registerUserSchema>;
+export type LoginInput = z.infer<typeof loginUserSchema>;
