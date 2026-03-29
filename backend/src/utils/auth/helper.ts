4bc7ee88473175bebefecd375a8e9ebc0a929ecd
Himanshu Tiwari <htiwari578@gmail.com>
2026-03-29 16:29:17 +0530

    implemented set auth cookies

---
+++ backend/src/utils/auth/helper.ts
@@ -0,0 +1,18 @@
+import { Response } from "express"
+
+export const setAuthCookies =(
+    res: Response,
+    accessToken: string,
+    refreshToken: string
+) => {
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: true,
+        maxAge: 15 * 60 * 1000, // 15 minutes
+    });
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+}
