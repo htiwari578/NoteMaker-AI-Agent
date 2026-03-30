4ad94bc023eeb5dcea0795716b5c2ee60bf927ac
Himanshu Tiwari <htiwari578@gmail.com>
2026-03-30 23:41:05 +0530

    logout routes

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { registerUser, userLogin } from "../controllers/user.controllers.js";
+import { registerUser, userLogin, userLogout } from "../controllers/user.controllers.js";
 import { validateData } from "../middleware/validate.middleware.js";
 import { loginUserSchema, registerUserSchema } from "../validations/auth.validations.js";
+import { verifyJwt } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.route("/register").post(validateData(registerUserSchema), registerUser);
 router.route("/login").post(validateData(loginUserSchema), userLogin);
+router.route("/logout").get(verifyJwt, userLogout);
 export default router;
