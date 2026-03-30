2669c21826ab7107f8d87c7e39b7ad44098b8965
Himanshu Tiwari <htiwari578@gmail.com>
2026-03-30 22:44:53 +0530

    implemented login routes

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import { registerUser, userLogin } from "../controllers/user.controllers.js";
 import { validateData } from "../middleware/validate.middleware.js";
-import { registerUserSchema } from "../validations/auth.validations.js";
+import { loginUserSchema, registerUserSchema } from "../validations/auth.validations.js";
 
 const router = express.Router();
 
 router.route("/register").post(validateData(registerUserSchema), registerUser);
+router.route("/login").post(validateData(loginUserSchema), userLogin);
 export default router;
