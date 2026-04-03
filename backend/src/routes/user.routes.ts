ec1a5a2c0ca9c5ac97462d2273a499d836674e0d
Himanshu Tiwari <htiwari578@gmail.com>
2026-04-03 22:53:15 +0530

    feat:added routes for fetching user profile

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser, userLogin, userLogout } from "../controllers/user.controllers.js";
+import { getCurrentUser, registerUser, userLogin, userLogout } from "../controllers/user.controllers.js";
 import { validateData } from "../middleware/validate.middleware.js";
 import { loginUserSchema, registerUserSchema } from "../validations/auth.validations.js";
 import { verifyJwt } from "../middleware/auth.middleware.js";
@@ -9,4 +9,5 @@ const router = express.Router();
 router.route("/register").post(validateData(registerUserSchema), registerUser);
 router.route("/login").post(validateData(loginUserSchema), userLogin);
 router.route("/logout").get(verifyJwt, userLogout);
+router.route("/current-user").get(verifyJwt, getCurrentUser);
 export default router;
