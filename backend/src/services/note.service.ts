7fc276f7e0b455dd7c971e0a8dd2d0d085254d93
Himanshu Tiwari <htiwari578@gmail.com>
2026-04-12 14:03:45 +0530

    feat:added database operation for notes

--- backend/src/services/note.service.ts
+++ backend/src/services/note.service.ts
@@ -1,6 +1,8 @@
 import { prisma } from "../lib/prisma.js";
 import { normalize } from "../utils/normalize.js";
 export const noteService = {
+
+  //fetch all notes
   async getAll(userId: string) {
     return prisma.note.findMany({
       where: {
@@ -11,6 +13,7 @@ export const noteService = {
       },
     });
   },
+  //insert new note
    async create(userId: string, content: string) {
     return prisma.note.create({
       data: {
@@ -19,6 +22,7 @@ export const noteService = {
       },
     });
   },
+  //full-text search by content
     async search(userId: string, query: string) {
     return prisma.note.findMany({
       where: {
@@ -35,6 +39,7 @@ export const noteService = {
       },
     });
   },
+  // exact match after normalizing
    async findByContent(userId: string, content: string) {
     const notes = await prisma.note.findMany({
       where: { userId },
@@ -43,5 +48,68 @@ export const noteService = {
     const target = normalize(content);
 
     return notes.find((note) => normalize(note.content) === target);
+  },
+  //filter inCompleted: true
+  async searchCompletedNotes(userId: string) {
+    return prisma.note.findMany({
+      where: {
+        userId,
+        isCompleted: true,
+      },
+      select: {
+        id: true,
+        content: true,
+        isCompleted: true,      
+      },
+    });
+  },
+  // filter isCompleted: false
+  async searchIncompletedNotes(userId: string) {
+    return prisma.note.findMany({
+      where: {
+        userId,
+        isCompleted: false,
+      },
+      select: {
+        id: true,
+        content: true,
+        isCompleted: true,
+      },
+    });
+  },
+
+  //edit note content
+
+  async update (noteId: string, userId: string, content: string) {
+    return prisma.note.updateMany({
+      where: {
+        id: noteId,
+        userId,
+      },
+      data: {
+        content,
+      },
+    });
+  },
+  //mark note completed
+  async complete(noteId: string, userId: string) {
+    return prisma.note.updateMany({
+      where: {
+        id: noteId,
+        userId,
+      },
+      data: {
+        isCompleted: true,
+      },
+    });
+  },
+  //delete note
+  async delete(noteId: string, userId: string) {
+    return prisma.note.deleteMany({
+      where: {
+        id: noteId,
+        userId,
+      },
+    });
   },
 }
